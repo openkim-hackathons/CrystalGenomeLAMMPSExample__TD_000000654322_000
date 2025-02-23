e0617f00209d60d73162d8e9bde42fedc71b6542
ilia Nikiforov <nikif002@umn.edu>
2025-02-22 21:02:52 -0600

    [no-ci] backup commit

--- test_driver/test_driver.py
+++ test_driver/test_driver.py
@@ -6,10 +6,10 @@ This is an example demonstrating usage of the kim-tools package. See https://kim
 
 """
 
-from kim_tools import CrystalGenomeTestDriver
+from kim_tools import SingleCrystalTestDriver
 from kim_tools import get_stoich_reduced_list_from_prototype
 
-class TestDriver(CrystalGenomeTestDriver):
+class TestDriver(SingleCrystalTestDriver):
     def _calculate(self, max_volume_scale: float = 1e-2, num_steps: int = 10, **kwargs):
         """
         Computes the energy vs. volume curve for isotropic expansion and compression. 
@@ -20,17 +20,25 @@ class TestDriver(CrystalGenomeTestDriver):
             num_steps:
                 Number of steps to take in each direction
         """
+        # The base class provides self._get_nominal_atoms(), which returns a copy of the internal Atoms object (not accessible directly).
+        # This Atoms object is a primitive cell in the setting defined in https://doi.org/10.1016/j.commatsci.2017.01.017
+        # Perform your calculations using this copy. If using ASE, the calculator is already attached.
+        atoms = self._get_nominal_atoms()
+        original_cell = atoms.get_cell()
+        num_atoms = len(atoms)
 
-        # The base class provides self.atoms, an ase.Atoms object representing the initial configuration of the crystal.
-        # Use this configuration to evaluate the material property of interest
-        original_cell = self.atoms.get_cell()
-        num_atoms = len(self.atoms)
-
-        # Besides temperature, stress, and atoms, you may wish to access other attributes of the base class for information about 
-        # the material, such as its symmetry-reduced AFLOW prototype label. Here we use it to get information about the stoichiometry of the crystal.
-        # See the API documentation for CrystalGenomeTestDriver for more information:
-        # https://kim-tools.readthedocs.io/en/latest/kim_tools.html#kim_tools.CrystalGenomeTestDriver
-        num_atoms_in_formula = sum(get_stoich_reduced_list_from_prototype(self.prototype_label))
+        # The base class also provides an instance of the OpenKIM `crystal-structure-npt` property (not accessible directly).
+        # This is a symmetry-reduced description of the crystal structure that is kept in sync with the internal Atoms object.
+        # See the definition of this property at 
+        # https://openkim.org/properties/show/2023-02-21/staff@noreply.openkim.org/crystal-structure-npt
+        # For a general explanation of the structure of a KIM Property Instance, see
+        # https://openkim.org/doc/schema/properties-framework/
+        # To access a copy of the property instance as a Python dictionary, use `self._get_nominal_crystal_structure_npt()`
+        
+        # Here we will use the prototype label to infer the number of atoms per stoichiometric formula
+        prototype_label = self._get_nominal_crystal_structure_npt()['prototype-label']['source-value']
+        
+        num_atoms_in_formula = sum(get_stoich_reduced_list_from_prototype(prototype_label))
 
         binding_potential_energy_per_atom = []
         binding_potential_energy_per_formula = []
@@ -44,8 +52,8 @@ class TestDriver(CrystalGenomeTestDriver):
         for i in range(-num_steps,num_steps+1):
             volume_scale = 1 + step_size*i
             linear_scale = volume_scale ** (1/3)
-            self.atoms.set_cell(original_cell*linear_scale,scale_atoms=True)
-            volume = self.atoms.get_volume()
+            atoms.set_cell(original_cell*linear_scale,scale_atoms=True)
+            volume = atoms.get_volume()
             current_volume_per_atom = volume/num_atoms
 
             problem_occurred = False
@@ -54,7 +62,7 @@ class TestDriver(CrystalGenomeTestDriver):
                 # the Test Driver was called with. If you need to access the name of the KIM model (for example,
                 # if you are exporting the atomic configuration to run an external simulator like LAMMPS), it can
                 # be accessed at self.kim_model_name
-                potential_energy = self.atoms.get_potential_energy()                
+                potential_energy = atoms.get_potential_energy()                
                 print('Volume: %5.5f Energy: %5.5f'%(volume,potential_energy))
             except:
                 print('Failed to get energy at volume %f'%volume)
@@ -70,6 +78,20 @@ class TestDriver(CrystalGenomeTestDriver):
 
         # Now it is time to write the output in the format you created in your Property Definition. The base class provides utility methods
         # to facilitate this process.
+        
+        # If your Test Driver changes the nominal crystal structure (e.g. through relaxation or MD), you must update the nominal
+        # crystal structure before writing properties. This is done by providing an Atoms object to self._update_nominal_crystal_structure().
+        # SingleCrystalTestDriver expects the crystal to maintain the same space group and occupied Wyckoff positions, meaning only
+        # the free parameters of the crystal unconstrained by symmetry are allowed to change. You must provide an Atoms object that
+        # is a primitive cell in the same setting. Translations, permutations, and rigid rotations of the unit cell are permissible,
+        # but the identity of the lattice vectors may not change during the property computation (e.g. lattice vectors a and c may
+        # not exchange places, even if allowed by symmetry)
+        
+        # This Test Driver does not actually need to do this, because the nominal structure does not change -- the energy-vs-volume
+        # curve is defined w.r.t. the original undeformed structure. For demonstration, we return the atoms object to its original 
+        # cell and update
+        atoms.set_cell(original_cell,scale_atoms=True)
+        self._update_nominal_crystal_structure
         
         # This method initializes the Property Instance and adds the keys common to all Crystal Genome properties.
         # property_name can be the full "property-id" field in your Property Definition, or the "Property Name",

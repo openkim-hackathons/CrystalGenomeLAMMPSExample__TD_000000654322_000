5fc8dbaa1a5edfb3065372fbe04b296c9ab72a3e
ilia Nikiforov <nikif002@umn.edu>
2025-07-24 22:25:31 +0000

    update to work with universal lj

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ Ultra-minimal example of running a LAMMPS Test Driver
 from test_driver.test_driver import TestDriver
 from ase.build import bulk
 
-kim_model_name = "Sim_LAMMPS_ReaxFF_RaymandVanDuinBaudin_2008_ZnOH__SM_449472104549_001"
+kim_model_name = "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003"
 test_driver = TestDriver(kim_model_name)
-atoms = bulk("Zn")
+atoms = bulk("Au")
 test_driver(atoms, lmp_cmd="mpirun -np 2 lmp")
 
 test_driver.write_property_instances_to_file()
